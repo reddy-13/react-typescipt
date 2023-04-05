f0f2fc3174a769a481930b94539c9ffec77cfe66
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2023-04-05 14:23:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import Container from './components/Container';
+import { Greet } from './components/Greet';
+import Heading from './components/Heading';
+import { Input } from './components/Input';
+import Oscar from './components/Oscar';
+import { Person } from './components/Person';
+import PersonList from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
+  // const personName = {
+  //   first: 'Bruce',
+  //   last: 'Wayne'
+  // }
+
+  // const nameLIst = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Kent'
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana'
+  //   }
+  // ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Status status='loading' />
+      <Heading>
+        Hehdsjhsjh
+      </Heading>
+      <Oscar>
+        <h2>Ocsar goes to goutham</h2>
+      </Oscar>
+      <Greet name='GGGG'  isLoggedIn={true} />
+      <Button handleClick={(event, id) => {
+        console.log(`Button Clicked`, event, id)
+      }} />
+      <Input value='' handleChange={event => console.log(event.target.value)} /> */}
+      <Container styles={{border: '2px solid red', padding: '1rem', color: 'orange'}} />
     </div>
   );
-}
+}   
 
 export default App;
